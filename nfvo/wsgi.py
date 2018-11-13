3dd137ffe0d07e9d06af30cc58b79c8a368567e8
Bernhard Wagesreiter <bernhard.wagesreiter@gmail.com>
2018-11-13 13:52:04 +0100

    initial commit

--- nfvo/wsgi.py
+++ nfvo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nfvo project.
+WSGI config for nfvoxx project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nfvo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nfvoxx.settings')
 
 application = get_wsgi_application()
